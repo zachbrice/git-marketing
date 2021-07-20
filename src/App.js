89d65684c43f942e4e5a47aa23178eda28ea7949
zachbrice <zbrice@banyanlabs.io>
2021-07-20 11:37:09 -0700

    market

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Zachs Marketplace</h1>
+     <div className="container">
+       <div className="item"><image></image></div>
+       <div className="item">sdgssg</div>
+       <div className="item">thdhds</div>
+     </div>
+       </div>
+   
+  );
+}
+
+export default App;
